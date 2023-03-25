506d5b6e975daf804d837bbcf94471e944c6351d
Arch <wbhaoui@gmail.com>
2023-03-25 21:38:44 +0000

    Add game over checking methods

--- main.js
+++ main.js
@@ -1,15 +1,69 @@
 // modules and factories
-const Gameboard = (() => {
+const gameBoard = (() => {
   let _state = [[], [], []];
-  const update_board = (row, column, symbol) => {
+  const _isFull = () => {
+    const fullCellCount = _state.reduce(
+      (length, row) => length + row.length,
+      0
+    );
+    return fullCellCount === 9;
+  };
+
+  const _horizontalCheck = () => {
+    for (let i = 0; i < 3; i++) {
+      if (
+        _state[i][0] === _state[i][1] &&
+        _state[i][0] === _state[i][2] &&
+        _state[i][0] !== undefined
+      ) {
+        return true;
+      } else {
+        continue;
+      }
+    }
+    return false;
+  };
+  const _verticalCheck = () => {
+    for (let i = 0; i < 3; i++) {
+      if (
+        _state[0][i] === _state[1][i] &&
+        _state[0][i] === _state[2][i] &&
+        _state[0][i] !== undefined
+      ) {
+        return true;
+      } else {
+        continue;
+      }
+    }
+    return false;
+  };
+  const _diagonalCheck = () => {
+    return (
+      (_state[0][0] === _state[1][1] &&
+        _state[0][0] === _state[2][2] &&
+        _state[0][0] !== undefined) ||
+      (_state[0][2] === _state[1][1] &&
+        _state[0][2] === _state[2][0] &&
+        _state[0][2] !== undefined)
+    );
+  };
+  const updateBoard = (row, column, symbol) => {
     _state[row][column] = symbol;
   };
-  return { _state, update_board };
+  const isOver = () => {
+    return (
+      _diagonalCheck() || _horizontalCheck() || _verticalCheck() || _isFull()
+    );
+  };
+  return {
+    updateBoard,
+    isOver,
+  };
 })();
 
 const displayController = (() => {
   const _gameGrid = document.getElementById("game_grid");
-  const initalize_display = () => {
+  const initalizeDisplay = () => {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         const cell = document.createElement("div");
@@ -19,15 +73,15 @@ const displayController = (() => {
       }
     }
   };
-  const update_display = (row, column, symbol) => {
+  const updateDisplay = (row, column, symbol) => {
     cellToUpdate = _gameGrid.querySelector(
       `[data-row="${row}"][data-column="${column}"]`
     );
     cellToUpdate.textContent = symbol;
   };
 
-  return { initalize_display, update_display };
+  return { initalizeDisplay, updateDisplay };
 })();
 
 // global code
-displayController.initalize_display();
+displayController.initalizeDisplay();
